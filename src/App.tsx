d1b1d0f7629805cc9bb0676b5ba93d3df722d667
imad ghazaz <107551738+imadghazaz@users.noreply.github.com>
2025-07-22 12:52:31 +0100

    Update App.tsx fix page name

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import AsyncForm from "./pages/AsyncForm";
-import SyncForm from "./pages/syncForm";
+import SyncForm from "./pages/SyncForm";
 
 const queryClient = new QueryClient();
 
